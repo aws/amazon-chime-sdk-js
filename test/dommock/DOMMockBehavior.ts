66c6b87243fbf8036fa744c3d60e2f4a02a45ec9
Kyu Simm <simmkyu@amazon.com>
2019-06-13 16:00:58 -0700

    Replace "console.log" with Logger and remove the "chai-as-promised" dependency

---
+++ test/dommock/DOMMockBehavior.ts
@@ -0,0 +1,50 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export default class DOMMockBehavior {
+  asyncWaitMs: number = 10;
+  getUserMediaSucceeds: boolean = true;
+  getUserMediaAudioLabel: string = 'Default';
+  webSocketOpenSucceeds: boolean = true;
+  webSocketSendSucceeds: boolean = true;
+  webSocketCloseSucceeds: boolean = true;
+  webSocketSendEcho: boolean = true;
+  iceConnectionStates: string[] = ['completed'];
+  FakeTURNCredentialsBody: Promise<object> = new Promise((resolve, _reject) => {
+    let obj: object = new Object({
+      username: 'fakeUsername',
+      password: 'fakeTURNCredentials',
+      ttl: Infinity,
+      uris: ['fakeUDPURI', 'fakeTCPURI'],
+    });
+    resolve(obj);
+  });
+  devicePixelRatio: number | null = 1;
+  setLocalDescriptionSucceeds: boolean = true;
+  setRemoteDescriptionStreamId: string = 'bc20510c2a134aa798f4dc9982f7c4a3adk';
+  setRemoteDescriptionNumberOfTracks: number = 1;
+  setRemoteDescriptionAddTrackSucceeds: boolean = true;
+  mediaStreamTrackCapabilities: { width: number; height: number } = {
+    width: 0,
+    height: 0,
+  };
+
+  mediaStreamTrackSettings: { width: number; height: number; deviceId: string } = {
+    width: 0,
+    height: 0,
+    deviceId: 'test',
+  };
+
+  deviceCounter: number = 0;
+  enumerateDevicesSucceeds: boolean = true;
+  enumerateDevicesSupported: boolean = true;
+  enumerateAudioOutputDeviceSupported: boolean = true;
+  mediaDeviceInfoSupported: boolean = true;
+  rtcPeerConnectionGetStatsSucceeds: boolean = true;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  rtcPeerConnectionGetStatsReport: { [name: string]: any } = {};
+  rtcPeerConnectionCreateOfferIncludesLocalHost: boolean = false;
+
+  browserName: string = 'firefox';
+  fetchSucceeds: boolean = true;
+}
