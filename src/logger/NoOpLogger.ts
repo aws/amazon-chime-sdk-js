817c0655b52faaad683455f3be4afd9bf679561f
Kyu Simm <simmkyu@amazon.com>
2019-06-13 19:48:09 -0700

    Replace DOMMock with sinon and add NoOpLogger for quiet testing

---
+++ src/logger/NoOpLogger.ts
@@ -0,0 +1,37 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import Logger from './Logger';
+import LogLevel from './LogLevel';
+
+/**
+ * [[NoOpLogger]] does not log any message.
+ */
+export default class NoOpLogger implements Logger {
+  level: LogLevel;
+
+  constructor(level = LogLevel.OFF) {
+    this.level = level;
+  }
+
+  info(_msg: string): void {}
+
+  warn(_msg: string): void {}
+
+  error(_msg: string): void {}
+
+  debug(debugFunction: () => string): void {
+    if (LogLevel.DEBUG < this.level) {
+      return;
+    }
+    debugFunction();
+  }
+
+  setLogLevel(level: LogLevel): void {
+    this.level = level;
+  }
+
+  getLogLevel(): LogLevel {
+    return this.level;
+  }
+}
