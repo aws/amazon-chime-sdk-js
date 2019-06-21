3c14e9e4e97f74fb48ff2b08a6af34c7d77c6928
Andrew Burnson <aburnson@amazon.com>
2019-06-21 15:38:40 -0700

    Add code style check for missing interface method comments

---
+++ src/logger/Logger.ts
@@ -0,0 +1,41 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import LogLevel from './LogLevel';
+
+/**
+ * [[Logger]] defines how to write logs for different logging level.
+ */
+export default interface Logger {
+  /**
+   * Calls [[debugFunction]] only if the log level is debug and emits the
+   * resulting string. Use the debug level to dump large or verbose messages
+   * that could slow down performance.
+   */
+  debug(debugFunction: () => string): void;
+
+  /**
+   * Emits an info message if the log level is equal to or lower than info level.
+   */
+  info(msg: string): void;
+
+  /**
+   * Emits a warning message if the log level is equal to or lower than warn level.
+   */
+  warn(msg: string): void;
+
+  /**
+   * Emits an error message if the log level is equal to or lower than error level.
+   */
+  error(msg: string): void;
+
+  /**
+   * Sets the log level.
+   */
+  setLogLevel(level: LogLevel): void;
+
+  /**
+   * Gets the current log level.
+   */
+  getLogLevel(): LogLevel;
+}
---
+++ src/pingpongobserver/PingPongObserver.ts
@@ -0,0 +1,20 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * [[PingPongObserver]] implements callbacks for when [[PingPong]] pongs are
+ * received or missed.
+ */
+export default interface PingPongObserver {
+  /**
+   * Called when a pong is received with the measured latency from the ping as
+   * well as an estimate of the local clock skew compared to the server clock.
+   */
+  didReceivePong?(id: number, latencyMs: number, clockSkewMs: number): void;
+
+  /**
+   * Called when one or more pongs are missed in a row. This can be an
+   * indication that the underlying connection has failed.
+   */
+  didMissPongs?(consecutiveMissed: number): void;
+}
