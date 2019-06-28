78b0b23055ff8e2f72fa0e89c9c1bc6fefb842bd
Dylon Dickinson <ddylon@amazon.com>
2019-06-27 20:20:56 -0700

    Add implementation and tests for ReceiveTURNCredentialsTask

---
+++ src/meetingsession/MeetingSessionTURNCredentials.ts
@@ -0,0 +1,12 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * [[MeetingSessionTURNCredentials]] contains TURN credentials from the TURN server.
+ */
+export default class MeetingSessionTURNCredentials {
+  username: string | null = null;
+  password: string | null = null;
+  ttl: number | null = null;
+  uris: string[] | null = null;
+}
