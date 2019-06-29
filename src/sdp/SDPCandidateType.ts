89ef80ed513d1b1eaa6c32e09df73077580e3256
Xuan He <hxuan@amazon.com>
2019-06-28 22:28:32 -0700

    Add SetRemoteDescriptionTask and unit test

---
+++ src/sdp/SDPCandidateType.ts
@@ -0,0 +1,11 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export enum SDPCandidateType {
+  Host = 'host',
+  ServerReflexive = 'srflx',
+  PeerReflexive = 'prflx',
+  Relay = 'relay',
+}
+
+export default SDPCandidateType;
