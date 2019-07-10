978b6d56e749bb69da4d100a437a303b738a6a1f
Kyu Simm <simmkyu@amazon.com>
2019-07-10 14:06:53 -0700

    Add task status to enforce that a task fails if it is already running or was finished

---
+++ src/task/TaskStatus.ts
@@ -0,0 +1,11 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+enum TaskStatus {
+  IDLE = 'IDLE',
+  RUNNING = 'RUNNING',
+  CANCELED = 'CANCELED',
+  FINISHED = 'FINISHED',
+}
+
+export default TaskStatus;
