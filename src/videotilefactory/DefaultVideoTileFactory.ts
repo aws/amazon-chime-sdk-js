f84c52ed4942f51727c81c8665f2186a5af00d4e
Kyu Simm <simmkyu@amazon.com>
2019-07-23 18:42:42 -0700

    Add VideoAdaptiveSubscribePolicy and other components that depend on the video tile controller

---
+++ src/videotilefactory/DefaultVideoTileFactory.ts
@@ -0,0 +1,19 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import DevicePixelRatioMonitor from '../devicepixelratiomonitor/DevicePixelRatioMonitor';
+import DefaultVideoTile from '../videotile/DefaultVideoTile';
+import VideoTile from '../videotile/VideoTile';
+import VideoTileController from '../videotilecontroller/VideoTileController';
+import VideoTileFactory from './VideoTileFactory';
+
+export default class DefaultVideoTileFactory implements VideoTileFactory {
+  makeTile(
+    tileId: number,
+    localTile: boolean,
+    tileController: VideoTileController,
+    devicePixelRatioMonitor: DevicePixelRatioMonitor
+  ): VideoTile {
+    return new DefaultVideoTile(tileId, localTile, tileController, devicePixelRatioMonitor);
+  }
+}
