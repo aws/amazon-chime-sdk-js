9c4b32498726448eafcd33e3b53b79604b069143
Geoff Little <littlegl@amazon.com>
2019-07-25 17:49:08 -0500

    Implement first layer of screen viewing setup-message handling

---
+++ src/screenviewing/jpegdecoder/ScreenViewingJpegDecoder.ts
@@ -0,0 +1,7 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export default interface ScreenViewingJpegDecoder {
+  //
+}
