170fc45236774dd6c56bd4cdacd5fe8ddad7c441
Geoff Little <littlegl@amazon.com>
2019-07-26 03:54:35 -0500

    Use factory pattern for screen viewing components

---
+++ src/screenviewing/deltarenderer/ScreenViewingDeltaRenderer.ts
@@ -0,0 +1,39 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import PresentationPolicy from '../../presentation/policy/PresentationPolicy';
+import ScreenViewingImageDimensions from '../messagehandler/ScreenViewingImageDimensions';
+
+export default interface ScreenViewingDeltaRenderer {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  syncBuffer: Uint8Array[][];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  jpegDataArrays: Uint8Array[][];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  hasRendered: boolean[][];
+  imageDimensions: ScreenViewingImageDimensions;
+  lastResizeAndSyncTime: number;
+
+  /**
+   * Builds the viewer.
+   * @param imageDimensions
+   */
+  buildViewer(imageDimensions: ScreenViewingImageDimensions): void;
+
+  /**
+   * Syncs the renderer.
+   */
+  resizeAndSync(): void;
+
+  setViewport(viewport: HTMLElement): void;
+
+  changePresentationPolicy(policy: PresentationPolicy): void;
+
+  close(): void;
+
+  zoomRelative(relativeZoomFactor: number): void;
+
+  zoomAbsolute(absoluteZoomFactor: number): void;
+
+  zoomReset(): void;
+}
