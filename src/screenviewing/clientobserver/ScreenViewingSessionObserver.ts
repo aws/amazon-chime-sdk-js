401b26d2b4a7dda2d254ebf21ff5ba36ce00cf47
Geoff Little <littlegl@amazon.com>
2019-08-15 11:15:24 -0700

    Migrate screen viewing connection to PromisedWebSocket

---
+++ src/screenviewing/clientobserver/ScreenViewingSessionObserver.ts
@@ -0,0 +1,20 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Instances of [[ScreenViewingSessionObserver]] can be registered with a
+ * [[ScreenViewingSession]] to receive notifications of events.
+ */
+export default interface ScreenViewingSessionObserver {
+  /**
+   * Notifies when the WebSocket is closed.
+   * @param event
+   */
+  didCloseWebSocket(event: CloseEvent): void;
+
+  /**
+   * Notifies when the WebSocket has received a message.
+   * @param event
+   */
+  didReceiveWebSocketMessage(event: MessageEvent): void;
+}
---
+++ src/screenviewing/session/DefaultScreenViewingSession.ts
@@ -0,0 +1,65 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import Logger from '../../logger/Logger';
+import Maybe from '../../maybe/Maybe';
+import PromisedWebSocket from '../../promisedwebsocket/PromisedWebSocket';
+import ReconnectingPromisedWebSocketFactory from '../../promisedwebsocket/ReconnectingPromisedWebSocketFactory';
+import ScreenViewingSessionObserver from '../clientobserver/ScreenViewingSessionObserver';
+import ScreenViewingSession from './ScreenViewingSession';
+import ScreenViewingSessionConnectionRequest from './ScreenViewingSessionConnectionRequest';
+
+/**
+ * [[DefaultScreenViewingSession]] is a default impl of the interface.
+ */
+export default class DefaultScreenViewingSession implements ScreenViewingSession {
+  private static readonly DEFAULT_TIMEOUT: number = 1000;
+  private webSocket?: PromisedWebSocket;
+  private observer?: ScreenViewingSessionObserver;
+
+  constructor(
+    private webSocketFactory: ReconnectingPromisedWebSocketFactory,
+    private logger: Logger
+  ) {}
+
+  withObserver(observer: ScreenViewingSessionObserver): ScreenViewingSession {
+    this.observer = observer;
+    return this;
+  }
+  openConnection(request: ScreenViewingSessionConnectionRequest): Promise<Event> {
+    this.logger.info(`DefaultScreenViewingSession: Opening connection`);
+    if (this.webSocket) {
+      return Promise.reject(new Error('Must close existing connection before opening a new one'));
+    }
+    this.webSocket = this.webSocketFactory.create(
+      request.screenViewingURL,
+      request.protocols(),
+      'arraybuffer'
+    );
+
+    this.webSocket.addEventListener('message', (event: MessageEvent) => {
+      Maybe.of(this.observer.didReceiveWebSocketMessage).map(f => f.bind(this.observer)(event));
+    });
+
+    this.webSocket.addEventListener('close', (event: CloseEvent) => {
+      Maybe.of(this.observer.didCloseWebSocket).map(f => f.bind(this.observer)(event));
+    });
+
+    return this.webSocket.open(request.timeoutMs);
+  }
+
+  closeConnection(): Promise<void> {
+    this.logger.info(`DefaultScreenViewingSession: Closing connection`);
+    if (!this.webSocket) {
+      return Promise.reject(new Error('No websocket to close'));
+    }
+    return this.webSocket.close(DefaultScreenViewingSession.DEFAULT_TIMEOUT).then((): void => {
+      this.webSocket = null;
+    });
+  }
+
+  send(data: Uint8Array): void {
+    this.logger.debug(() => `DefaultScreenViewingSession: Sending ${data.byteLength} bytes`);
+    this.webSocket.send(data);
+  }
+}
