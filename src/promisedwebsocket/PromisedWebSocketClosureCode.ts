c531fbfb551db23ee4c1fce7b78c644a05c29d13
Aaron Gowatch <agowatch@amazon.com>
2019-08-26 17:05:23 -0600

    Add closure callback to websocket implementation

---
+++ src/promisedwebsocket/PromisedWebSocketClosureCode.ts
@@ -0,0 +1,9 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export enum PromisedWebSocketClosureCode {
+  Normal = 1000,
+  EmptyCloseFrame = 1005,
+}
+
+export default PromisedWebSocketClosureCode;
