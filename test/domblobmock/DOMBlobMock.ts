a4cfab9a758fd707b1e4ab5026e8f931368ebff1
Aaron Gowatch <agowatch@amazon.com>
2019-08-27 17:28:18 -0600

    Remove asynchronous Blob conversion

---
+++ test/domblobmock/DOMBlobMock.ts
@@ -0,0 +1,18 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export default class DOMBlobMock implements Blob {
+  get size(): number {
+    return 0;
+  }
+
+  get type(): string {
+    return '';
+  }
+
+  constructor(readonly blobParts?: BlobPart[], readonly options?: BlobPropertyBag) {}
+
+  slice(_start?: number, _end?: number, _contentType?: string): Blob {
+    return new Blob();
+  }
+}
