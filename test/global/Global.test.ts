c5e0416cfcb158d6d227a72857fde790610b2b4e
Andrew Burnson <aburnson@amazon.com>
2019-08-28 17:23:13 -0700

    Add npm run test:slow to aid debugging racing tests

---
+++ test/global/Global.test.ts
@@ -0,0 +1,12 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+const mochaTestDelayMs = parseInt(process.env.MOCHA_TEST_DELAY_MS);
+
+if (mochaTestDelayMs > 0) {
+  beforeEach(done => {
+    setTimeout(() => {
+      done();
+    }, mochaTestDelayMs);
+  });
+}
