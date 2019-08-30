4868c789c56cf4aed8908d8b9969d8a33c2f3f65
Maggie Yang <ymengyun@amazon.com>
2019-08-30 13:56:39 -0700

    Add partial unit tests for active speaker detector

---
+++ test/activespeakerdetector/DefaultActiveSpeakerDetector.test.ts
@@ -0,0 +1,95 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as chai from 'chai';
+
+import DefaultActiveSpeakerDetector from '../../src/activespeakerdetector/DefaultActiveSpeakerDetector';
+import DefaultActiveSpeakerPolicy from '../../src/activespeakerpolicy/DefaultActiveSpeakerPolicy';
+import DefaultRealtimeController from '../../src/realtimecontroller/DefaultRealtimeController';
+
+describe('DefaultActiveSpeakerDetector', () => {
+  const expect: Chai.ExpectStatic = chai.expect;
+  const attendeeId = 'self';
+  const bandwidthPriorityCallback = (): void => {};
+
+  describe('construction', () => {
+    it('can be constructed', () => {
+      let rt = new DefaultRealtimeController();
+      let detector = new DefaultActiveSpeakerDetector(rt, attendeeId, bandwidthPriorityCallback);
+      expect(detector).to.not.equal(null);
+    });
+  });
+
+  describe('subscribeToActiveSpeakerDetector', () => {
+    it('can subscribe to detector', () => {
+      let rt = new DefaultRealtimeController();
+      let detector = new DefaultActiveSpeakerDetector(rt, attendeeId, bandwidthPriorityCallback);
+      let policy = new DefaultActiveSpeakerPolicy();
+      const callback = (_attendeeIds: string[]): void => {};
+      detector.subscribe(policy, callback);
+      detector.unsubscribe(callback);
+    });
+
+    it('attendee id change should trigger the active speaker detector', () => {
+      let rt = new DefaultRealtimeController();
+      let detector = new DefaultActiveSpeakerDetector(rt, attendeeId, bandwidthPriorityCallback);
+      let policy = new DefaultActiveSpeakerPolicy();
+      let callbackFired = false;
+      const fooAttendee = 'foo-attendee';
+      const callback = (_attendeeIds: string[]): void => {
+        callbackFired = true;
+      };
+      detector.subscribe(policy, callback);
+      rt.realtimeSetAttendeeIdPresence(fooAttendee, true);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 10, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 10, false, 1);
+      expect(callbackFired).to.be.true;
+      callbackFired = false;
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 10, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 0, false, 1);
+      expect(callbackFired).to.be.false;
+      rt.realtimeSetAttendeeIdPresence(fooAttendee, false);
+      expect(callbackFired).to.be.true;
+      detector.unsubscribe(callback);
+    });
+
+    it('can return sorted active speakers', () => {
+      let rt = new DefaultRealtimeController();
+      let detector = new DefaultActiveSpeakerDetector(rt, attendeeId, bandwidthPriorityCallback);
+      let policy = new DefaultActiveSpeakerPolicy();
+      const fooAttendee1 = 'foo-attendee';
+      const fooAttendee2 = 'foo-attendee2';
+      const callback = (attendeeIds: string[]): void => {
+        expect(attendeeIds.length === 2);
+        expect(attendeeIds[0] === fooAttendee2);
+      };
+      detector.subscribe(policy, callback);
+      rt.realtimeSetAttendeeIdPresence(fooAttendee1, true);
+      rt.realtimeSetAttendeeIdPresence(fooAttendee2, true);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee1, 10, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee1, 20, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee2, 10, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee2, 90, false, 1);
+      detector.unsubscribe(callback);
+    });
+  });
+
+  describe('unsubscribeFromActiveSpeakerDetector', () => {
+    it('can unsubscribe from detector', () => {
+      let rt = new DefaultRealtimeController();
+      let detector = new DefaultActiveSpeakerDetector(rt, attendeeId, bandwidthPriorityCallback);
+      let policy = new DefaultActiveSpeakerPolicy();
+      let callbackFired = false;
+      const fooAttendee = 'foo-attendee';
+      const callback = (_attendeeIds: string[]): void => {
+        callbackFired = true;
+      };
+      detector.subscribe(policy, callback);
+      detector.unsubscribe(callback);
+      rt.realtimeSetAttendeeIdPresence(fooAttendee, true);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 10, false, 1);
+      rt.realtimeUpdateVolumeIndicator(fooAttendee, 10, false, 1);
+      expect(callbackFired).to.be.false;
+    });
+  });
+});
---
+++ test/activespeakerpolicy/DefaultActiveSpeakerPolicy.test.ts
@@ -0,0 +1,53 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as chai from 'chai';
+
+import DefaultActiveSpeakerPolicy from '../../src/activespeakerpolicy/DefaultActiveSpeakerPolicy';
+
+describe('DefaultActiveSpeakerPolicy', () => {
+  const expect: Chai.ExpectStatic = chai.expect;
+
+  describe('construction', () => {
+    it('can be constructed', () => {
+      const policy = new DefaultActiveSpeakerPolicy();
+      expect(policy).to.not.equal(null);
+    });
+
+    it('can be constructed with different parameters', () => {
+      const policy = new DefaultActiveSpeakerPolicy(0.1, 0.9);
+      expect(policy).to.not.equal(null);
+    });
+  });
+
+  describe('calculateScore', () => {
+    it('can calculate active speaker score', () => {
+      const policy = new DefaultActiveSpeakerPolicy();
+      const attendeeId = 'foo-attendeeId';
+      const activeScore = policy.calculateScore(attendeeId, 0, false);
+      expect(activeScore).to.equal(0);
+    });
+
+    it('can return active speaker score if larger than threshold', () => {
+      const policy = new DefaultActiveSpeakerPolicy();
+      const attendeeId = 'foo-attendeeId';
+      policy.calculateScore(attendeeId, 10, false);
+      const activeScore = policy.calculateScore(attendeeId, 10, false);
+      expect(activeScore).to.not.equal(0);
+    });
+
+    it('can set volume as 0 if muted', () => {
+      const policy = new DefaultActiveSpeakerPolicy();
+      const attendeeId = 'foo-attendeeId';
+      const activeScore = policy.calculateScore(attendeeId, 1, true);
+      expect(activeScore).to.equal(0);
+    });
+  });
+
+  describe('prioritizeVideoSendBandwidthForActiveSpeaker', () => {
+    it('will return true', () => {
+      const policy = new DefaultActiveSpeakerPolicy();
+      expect(policy.prioritizeVideoSendBandwidthForActiveSpeaker()).to.equal(true);
+    });
+  });
+});
