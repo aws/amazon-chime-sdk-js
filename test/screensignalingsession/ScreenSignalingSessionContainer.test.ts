3e516d6b28eac7ba6995fd754db89e9700c394ae
Aaron Gowatch <agowatch@amazon.com>
2019-09-23 09:12:01 -0600

    Add ScreenSignalingSessionContainer

---
+++ test/screensignalingsession/ScreenSignalingSessionContainer.test.ts
@@ -0,0 +1,26 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Substitute } from '@fluffy-spoon/substitute';
+import * as chai from 'chai';
+
+import Logger from '../../src/logger/Logger';
+import ReconnectingPromisedWebSocketFactory from '../../src/promisedwebsocket/ReconnectingPromisedWebSocketFactory';
+import DefaultScreenSignalingSessionFactory from '../../src/screensignalingsession/DefaultScreenSignalingSessionFactory';
+import ScreenSignalingSessionContainer from '../../src/screensignalingsession/ScreenSignalingSessionContainer';
+
+describe('ScreenSignalingSessionContainer', () => {
+  describe('#create', () => {
+    it('is created', () => {
+      const webSocketFactory = Substitute.for<ReconnectingPromisedWebSocketFactory>();
+      const logger = Substitute.for<Logger>();
+      const subject = new ScreenSignalingSessionContainer(webSocketFactory, logger);
+      chai
+        .expect(subject.screenSignalingSessionFactory())
+        .to.be.instanceOf(DefaultScreenSignalingSessionFactory);
+      chai
+        .expect(subject.screenSignalingSessionFactory())
+        .to.be.instanceOf(DefaultScreenSignalingSessionFactory);
+    });
+  });
+});
