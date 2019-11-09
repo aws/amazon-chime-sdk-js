203f6bd0b2e39994024d69a31fed74f578074060
Andrew Burnson <aburnson@amazon.com>
2019-11-08 16:00:28 -0800

    Prioritize bandwidth when active speaker

---
+++ src/activespeakerdetector/DefaultActiveSpeakerDetector.ts
@@ -0,0 +1,176 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import ActiveSpeakerPolicy from '../activespeakerpolicy/ActiveSpeakerPolicy';
+import RealtimeController from '../realtimecontroller/RealtimeController';
+import IntervalScheduler from '../scheduler/IntervalScheduler';
+import ActiveSpeakerDetector from './ActiveSpeakerDetector';
+
+/**
+ * Implements the DefaultActiveSpeakerDetector with the [[ActiveSpeakerPolicy]]
+ */
+type DetectorCallback = (attendeeIds: string[]) => void;
+type DetectorHandler = (attendeeId: string, present: boolean) => void;
+
+export default class DefaultActiveSpeakerDetector implements ActiveSpeakerDetector {
+  private static ACTIVITY_WAIT_INTERVAL_MS = 1000;
+  private static ACTIVITY_UPDATE_INTERVAL_MS = 200;
+
+  private speakerScores: { [attendeeId: string]: number } = {};
+  private speakerMuteState: { [attendeeId: string]: boolean } = {};
+
+  private activeSpeakers: string[];
+  private detectorCallbackToHandler: Map<DetectorCallback, DetectorHandler> = new Map<
+    DetectorCallback,
+    DetectorHandler
+  >();
+  private detectorCallbackToScoresTimer: Map<DetectorCallback, IntervalScheduler> = new Map<
+    DetectorCallback,
+    IntervalScheduler
+  >();
+  private detectorCallbackToActivityTimer: Map<DetectorCallback, IntervalScheduler> = new Map<
+    DetectorCallback,
+    IntervalScheduler
+  >();
+  private hasBandwidthPriority = false;
+
+  private mostRecentUpdateTimestamp: { [attendeeId: string]: number } = {};
+
+  constructor(
+    private realtimeController: RealtimeController,
+    private selfAttendeeId: string,
+    private hasBandwidthPriorityCallback: (hasBandwidthPriority: boolean) => void
+  ) {}
+
+  private needUpdate(attendeeId: string): boolean {
+    if (!this.activeSpeakers) {
+      return true;
+    }
+    return (
+      (this.speakerScores[attendeeId] === 0 && this.activeSpeakers.includes(attendeeId)) ||
+      (this.speakerScores[attendeeId] > 0 && !this.activeSpeakers.includes(attendeeId))
+    );
+  }
+
+  private updateActiveSpeakers(
+    policy: ActiveSpeakerPolicy,
+    callback: DetectorCallback,
+    attendeeId: string
+  ): void {
+    if (!this.needUpdate(attendeeId)) {
+      return;
+    }
+    let sortedSpeakers: { attendeeId: string; activeScore: number }[] = [];
+
+    const attendeeIds = Object.keys(this.speakerScores);
+    for (let i = 0; i < attendeeIds.length; i++) {
+      const attendeeId = attendeeIds[i];
+      sortedSpeakers.push({ attendeeId: attendeeId, activeScore: this.speakerScores[attendeeId] });
+    }
+
+    let sortedAttendeeIds = sortedSpeakers
+      .sort((s1, s2) => s2.activeScore - s1.activeScore)
+      .filter(function(s) {
+        return s.activeScore > 0;
+      })
+      .map(function(s) {
+        return s.attendeeId;
+      });
+    this.activeSpeakers = sortedAttendeeIds;
+    callback(sortedAttendeeIds);
+    const selfIsActive =
+      sortedAttendeeIds.length > 0 && sortedAttendeeIds[0] === this.selfAttendeeId;
+    const hasBandwidthPriority =
+      selfIsActive && policy.prioritizeVideoSendBandwidthForActiveSpeaker();
+    const hasBandwidthPriorityDidChange = this.hasBandwidthPriority !== hasBandwidthPriority;
+    if (hasBandwidthPriorityDidChange) {
+      this.hasBandwidthPriority = hasBandwidthPriority;
+      this.hasBandwidthPriorityCallback(hasBandwidthPriority);
+    }
+  }
+
+  private updateScore(
+    policy: ActiveSpeakerPolicy,
+    callback: DetectorCallback,
+    attendeeId: string,
+    volume: number | null,
+    muted: boolean | null
+  ): void {
+    const activeScore = policy.calculateScore(attendeeId, volume, muted);
+    if (this.speakerScores[attendeeId] !== activeScore) {
+      this.speakerScores[attendeeId] = activeScore;
+      this.updateActiveSpeakers(policy, callback, attendeeId);
+    }
+  }
+
+  subscribe(
+    policy: ActiveSpeakerPolicy,
+    callback: DetectorCallback,
+    scoresCallback?: (scores: { [attendeeId: string]: number }) => void,
+    scoresCallbackIntervalMs?: number
+  ): void {
+    let handler = (attendeeId: string, present: boolean): void => {
+      if (!present) {
+        this.speakerScores[attendeeId] = 0;
+        this.updateActiveSpeakers(policy, callback, attendeeId);
+        return;
+      }
+      this.realtimeController.realtimeSubscribeToVolumeIndicator(
+        attendeeId,
+        (
+          attendeeId: string,
+          volume: number | null,
+          muted: boolean | null,
+          _signalStrength: number | null
+        ) => {
+          this.mostRecentUpdateTimestamp[attendeeId] = Date.now();
+          if (muted !== null) {
+            this.speakerMuteState[attendeeId] = muted;
+          }
+          this.updateScore(policy, callback, attendeeId, volume, muted);
+        }
+      );
+    };
+    this.detectorCallbackToHandler.set(callback, handler);
+
+    const activityTimer = new IntervalScheduler(
+      DefaultActiveSpeakerDetector.ACTIVITY_UPDATE_INTERVAL_MS
+    );
+    activityTimer.start(() => {
+      for (const attendeeId in this.speakerScores) {
+        const lastTimestamp = this.mostRecentUpdateTimestamp[attendeeId] || 0;
+        if (Date.now() - lastTimestamp > DefaultActiveSpeakerDetector.ACTIVITY_WAIT_INTERVAL_MS) {
+          this.updateScore(policy, callback, attendeeId, 0, this.speakerMuteState[attendeeId]);
+        }
+      }
+    });
+    this.detectorCallbackToActivityTimer.set(callback, activityTimer);
+
+    if (scoresCallback && scoresCallbackIntervalMs) {
+      const scoresTimer = new IntervalScheduler(scoresCallbackIntervalMs);
+      scoresTimer.start(() => {
+        scoresCallback(this.speakerScores);
+      });
+      this.detectorCallbackToScoresTimer.set(callback, scoresTimer);
+    }
+    this.realtimeController.realtimeSubscribeToAttendeeIdPresence(handler);
+  }
+
+  unsubscribe(callback: DetectorCallback): void {
+    const handler = this.detectorCallbackToHandler.get(callback);
+    this.detectorCallbackToHandler.delete(callback);
+    this.realtimeController.realtimeUnsubscribeToAttendeeIdPresence(handler);
+
+    const activityTimer = this.detectorCallbackToActivityTimer.get(callback);
+    if (activityTimer) {
+      activityTimer.stop();
+      this.detectorCallbackToActivityTimer.delete(callback);
+    }
+
+    const scoresTimer = this.detectorCallbackToScoresTimer.get(callback);
+    if (scoresTimer) {
+      scoresTimer.stop();
+      this.detectorCallbackToHandler.delete(callback);
+    }
+  }
+}
---
+++ src/activespeakerpolicy/DefaultActiveSpeakerPolicy.ts
@@ -0,0 +1,47 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import ActiveSpeakerPolicy from './ActiveSpeakerPolicy';
+
+export default class DefaultActiveSpeakerPolicy implements ActiveSpeakerPolicy {
+  private volumes: { [attendeeId: string]: number } = {};
+
+  constructor(
+    private speakerWeight: number = 0.9,
+    private cutoffThreshold: number = 0.01,
+    private silenceThreshold = 0.2,
+    private takeoverRate = 0.2
+  ) {}
+
+  calculateScore(attendeeId: string, volume: number | null, muted: boolean | null): number {
+    if (muted || volume === null) {
+      volume = 0;
+    }
+    if (!this.volumes.hasOwnProperty(attendeeId)) {
+      this.volumes[attendeeId] = 0;
+    }
+    if (volume > this.silenceThreshold) {
+      volume = 1.0;
+    } else {
+      volume = 0.0;
+    }
+    let score = this.volumes[attendeeId] * this.speakerWeight + volume * (1 - this.speakerWeight);
+    this.volumes[attendeeId] = score;
+    for (const otherAttendeeId in this.volumes) {
+      if (otherAttendeeId !== attendeeId) {
+        this.volumes[otherAttendeeId] = Math.max(
+          this.volumes[otherAttendeeId] - this.takeoverRate * volume,
+          0
+        );
+      }
+    }
+    if (score < this.cutoffThreshold) {
+      return 0;
+    }
+    return score;
+  }
+
+  prioritizeVideoSendBandwidthForActiveSpeaker(): boolean {
+    return true;
+  }
+}
