eb88c7cd592f6778125143f201e3c84e34a06875
Andrew Burnson <aburnson@amazon.com>
2019-11-09 20:04:31 -0800

    Add versioning to all network requests

---
+++ src/versioning/Versioning.ts
@@ -0,0 +1,37 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import DefaultBrowserBehavior from '../browserbehavior/DefaultBrowserBehavior';
+
+export default class Versioning {
+  static X_AMZN_VERSION = 'X-Amzn-Version';
+  static X_AMZN_USER_AGENT = 'X-Amzn-User-Agent';
+
+  /**
+   * Return string representation of SDK version
+   */
+  static get sdkVersion(): string {
+    return '0.9.0';
+  }
+
+  /**
+   * Return low-resolution string representation of SDK user agent (e.g. `chrome-78`)
+   */
+  static get sdkUserAgentLowResolution(): string {
+    const browserBehavior = new DefaultBrowserBehavior();
+    return `${browserBehavior.name()}-${browserBehavior.majorVersion()}`;
+  }
+
+  /**
+   * Return URL with versioning information appended
+   */
+  static urlWithVersion(url: string): string {
+    const urlWithVersion = new URL(url);
+    urlWithVersion.searchParams.append(Versioning.X_AMZN_VERSION, Versioning.sdkVersion);
+    urlWithVersion.searchParams.append(
+      Versioning.X_AMZN_USER_AGENT,
+      Versioning.sdkUserAgentLowResolution
+    );
+    return urlWithVersion.toString();
+  }
+}
