3b25f39ee0cfdfdba420ef7c98217303354cc1c7
Alex Snyatkov <snyatkov@amazon.com>
2019-11-15 08:01:08 -0800

    Improved deployment script, added js version

---
+++ demos/serverless/deploy.js
@@ -0,0 +1,122 @@
+const { spawnSync } = require('child_process');
+const fs = require("fs");
+const path = require("path");
+
+// Parameters
+let region = 'us-east-1';
+let bucket = ``;
+let stack = ``;
+let app = `meeting`;
+
+function usage() {
+  console.log(`Usage: deploy.sh [-r region] [-b bucket] [-s stack] [-a application]`);
+  console.log(`  -r, --region      Target region, default '${region}'`);
+  console.log(`  -b, --s3-bucket   S3 bucket for deployment, required`);
+  console.log(`  -s, --stack-name  CloudFormation stack name, required`);
+  console.log(`  -a, --application Browser application to deploy, default '${app}'`);
+  console.log(`  -h, --help        Show help and exit`);
+}
+
+function ensureBucket() {
+  const s3Api = spawnSync('aws', ['s3api', 'head-bucket', '--bucket', `${bucket}`, '--region', '${region}']);
+  if (s3Api.status !== 0) {
+    console.log(`Creating S3 bucket ${bucket}`);
+    const s3 = spawnSync('aws', ['s3', 'mb', `s3://${bucket}`, '--region', `${region}`]);
+    if (s3.status !== 0) {
+      console.log(s3.stderr.toString());
+      process.exit(s3.status)
+    }
+  }
+}
+
+function getArgOrExit(i, args) {
+  if (i >= args.length) {
+    console.log('Too few arguments');
+    usage();
+    process.exit(1);
+  }
+  return args[i];
+}
+
+function parseArgs() {
+  var args = process.argv.slice(2);
+  var i = 0;
+  while (i < args.length) {
+    switch(args[i]) {
+      case '-h': case '--help':
+        usage();
+        process.exit(0);
+        break;
+      case '-r': case '--region':
+        region = getArgOrExit(++i, args)
+        break;
+      case '-b': case '--s3-bucket':
+        bucket = getArgOrExit(++i, args)
+        break;
+      case '-s': case '--stack-name':
+        stack = getArgOrExit(++i, args)
+        break;
+      case '-a': case '--application':
+        app = getArgOrExit(++i, args)
+        break;
+      default:
+        console.log(`Invalid argument ${args[i]}`);
+        usage();
+        process.exit(1)
+    }
+    ++i;
+  }
+  if (!stack.trim() || !bucket.trim()) {
+    console.log('Missing required parameters');
+    usage();
+    process.exit(1);
+  }
+}
+
+function spawnOrFail(command, args, options) {
+  const cmd = spawnSync(command, args, options);
+  if (cmd.status !== 0) {
+    console.log(cmd.stderr.toString());
+    process.exit(cmd.status)
+  }
+  console.log(cmd.stdout.toString());
+}
+
+const appHtml = `../browser/dist/${app}.html`;
+function ensureApp() {
+  console.log(`Verifying application ${app}`);
+  if (!fs.existsSync(`../browser/app/${app}`)) {
+    console.log(`Application ${app} does not exist. Did you specify correct name?`);
+    process.exit(1);
+  }
+  if (!fs.existsSync(appHtml)) {
+    console.log(`Application ${appHtml} does not exist. Rebuilding demo apps`);
+    spawnOrFail('npm', ['run', 'build'], {cwd: path.join(__dirname, '..', 'browser')});
+  }
+
+  // TODO: remove this once AWS Lambda Node.js runtime includes the Chime APIs
+  spawnOrFail('npm', ['install', '--production'], {cwd: path.join(__dirname, '..', 'browser', 'node_modules', 'aws-sdk')});
+}
+
+parseArgs();
+ensureApp();
+
+if (!fs.existsSync('build')) {
+  fs.mkdirSync('build');
+}
+
+console.log(`Using region ${region}, bucket ${bucket}, stack ${stack}`);
+ensureBucket();
+
+  // TODO: remove this once AWS Lambda Node.js runtime includes the Chime APIs
+spawnOrFail('cp', ['-Rp', path.join(__dirname, '..', 'browser', 'node_modules', 'aws-sdk'), 'src']);
+
+spawnOrFail('cp', [appHtml, 'src/index.html']);
+spawnOrFail('sam', ['package', '--s3-bucket', `${bucket}`, `--output-template-file`, `build/packaged.yaml`,
+                     '--region',  `${region}`]);
+console.log('Deploying serverless application');
+spawnOrFail('sam', ['deploy', '--template-file', './build/packaged.yaml', '--stack-name', `${stack}`,
+                     '--capabilities', 'CAPABILITY_IAM', '--region', `${region}`]);
+console.log("Amazon Chime SDK Meeting Demo URL: ");
+spawnOrFail('aws', ['cloudformation', 'describe-stacks', '--stack-name', `${stack}`,
+                     '--query', 'Stacks[0].Outputs[0].OutputValue', '--output', 'text', '--region', `${region}`]);
