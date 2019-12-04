5c3731403c130ac25c84f21515df2224ae675388
Andrew Burnson <aburnson@amazon.com>
2019-12-03 16:26:36 -0800

    Add video help desk tutorial

---
+++ demos/videohelpdesk/webpack.config.js
@@ -0,0 +1,30 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable */
+var HtmlWebpackInlineSourcePlugin = require ('html-webpack-inline-source-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-enable */
+
+module.exports = env => {
+  const app = env.app;
+  return {
+    plugins: [
+      new HtmlWebpackPlugin({
+        inlineSource: '.(js)$',
+        template: __dirname + `/src/${app}.html`,
+        filename: __dirname + `/dist/${app}.html`,
+        inject: 'head',
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+    ],
+    entry: [`./src/${app}.js`],
+    resolve: {
+      extensions: ['.js'],
+    },
+    mode: 'development',
+    performance: {
+      hints: false,
+    },
+  };
+};
