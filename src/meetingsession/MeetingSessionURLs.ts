15c73c33de39507ac36d98bc2c68515ea5c7c87d
Andrew Burnson <aburnson@amazon.com>
2020-03-10 09:36:13 -0700

    Add injectable function to rewrite session URLs

--- src/meetingsession/MeetingSessionURLs.ts
+++ src/meetingsession/MeetingSessionURLs.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 /**
@@ -9,30 +9,122 @@ export default class MeetingSessionURLs {
   /**
    * The audio host URL of the session
    */
-  audioHostURL: string;
+  private _audioHostURL: string | null = null;
 
   /**
    * The screen data URL of the session
    */
-  screenDataURL: string;
+  private _screenDataURL: string | null = null;
 
   /**
    * The screen sharing URL of the session
    */
-  screenSharingURL: string;
+  private _screenSharingURL: string | null = null;
 
   /**
    * The screen viewing URL of the session
    */
-  screenViewingURL: string;
+  private _screenViewingURL: string | null = null;
 
   /**
    * The signaling URL of the session
    */
-  signalingURL: string;
+  private _signalingURL: string | null = null;
 
   /**
    * The TURN control URL of the session
    */
-  turnControlURL: string;
+  private _turnControlURL: string | null = null;
+
+  /**
+   * Gets the audio host URL after applying the urlRewriter function.
+   */
+  get audioHostURL(): string | null {
+    return this.urlRewriter(this._audioHostURL);
+  }
+
+  /**
+   * Sets the raw audio host URL.
+   */
+  set audioHostURL(value: string | null) {
+    this._audioHostURL = value;
+  }
+
+  /**
+   * Gets the screen data URL after applying the urlRewriter function.
+   */
+  get screenDataURL(): string | null {
+    return this.urlRewriter(this._screenDataURL);
+  }
+
+  /**
+   * Sets the raw screen data URL.
+   */
+  set screenDataURL(value: string | null) {
+    this._screenDataURL = value;
+  }
+
+  /**
+   * Gets the screen sharing URL after applying the urlRewriter function.
+   */
+  get screenSharingURL(): string | null {
+    return this.urlRewriter(this._screenSharingURL);
+  }
+
+  /**
+   * Sets the raw screen sharing URL.
+   */
+  set screenSharingURL(value: string | null) {
+    this._screenSharingURL = value;
+  }
+
+  /**
+   * Gets the screen viewing URL after applying the urlRewriter function.
+   */
+  get screenViewingURL(): string | null {
+    return this.urlRewriter(this._screenViewingURL);
+  }
+
+  /**
+   * Sets the raw screen viewing URL.
+   */
+  set screenViewingURL(value: string | null) {
+    this._screenViewingURL = value;
+  }
+
+  /**
+   * Gets the signaling URL after applying the urlRewriter function.
+   */
+  get signalingURL(): string | null {
+    return this.urlRewriter(this._signalingURL);
+  }
+
+  /**
+   * Sets the raw signaling URL.
+   */
+  set signalingURL(value: string | null) {
+    this._signalingURL = value;
+  }
+
+  /**
+   * Gets the TURN control URL after applying the urlRewriter function.
+   */
+  get turnControlURL(): string | null {
+    return this.urlRewriter(this._turnControlURL);
+  }
+
+  /**
+   * Sets the raw TURN control URL.
+   */
+  set turnControlURL(value: string | null) {
+    this._turnControlURL = value;
+  }
+
+  /**
+   * Function to transform URLs. Use this to rewrite URLs to traverse proxies.
+   * The default implementation returns the original URL unchanged.
+   */
+  urlRewriter: (url: string | null) => string | null = (url: string | null) => {
+    return url;
+  };
 }
---
+++ test/meetingsession/MeetingSessionURLs.test.ts
@@ -0,0 +1,45 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as chai from 'chai';
+
+import MeetingSessionURLs from '../../src/meetingsession/MeetingSessionURLs';
+
+describe('MeetingSessionURLs', () => {
+  const expect: Chai.ExpectStatic = chai.expect;
+
+  describe('constructor', () => {
+    it('can be constructed', () => {
+      const urls = new MeetingSessionURLs();
+      expect(urls.audioHostURL).to.be.null;
+      expect(urls.screenDataURL).to.be.null;
+      expect(urls.screenSharingURL).to.be.null;
+      expect(urls.screenViewingURL).to.be.null;
+      expect(urls.signalingURL).to.be.null;
+      expect(urls.turnControlURL).to.be.null;
+      expect(urls.urlRewriter(null)).to.be.null;
+      expect(urls.urlRewriter('foobar')).to.eq('foobar');
+    });
+
+    it('can use a custom URL rewriter', () => {
+      const urls = new MeetingSessionURLs();
+      urls.audioHostURL = 'audio-host-url';
+      urls.screenDataURL = 'screen-data-url';
+      urls.screenSharingURL = 'screen-sharing-url';
+      urls.screenViewingURL = 'screen-viewing-url';
+      urls.signalingURL = 'signaling-url';
+      urls.turnControlURL = 'turn-control-url';
+      urls.urlRewriter = (url: string | null) => {
+        return url !== null ? url.replace('url', 'rewritten-url') : null;
+      };
+      expect(urls.urlRewriter(null)).to.be.null;
+      expect(urls.urlRewriter('url')).to.eq('rewritten-url');
+      expect(urls.audioHostURL).to.eq('audio-host-rewritten-url');
+      expect(urls.screenDataURL).to.eq('screen-data-rewritten-url');
+      expect(urls.screenSharingURL).to.eq('screen-sharing-rewritten-url');
+      expect(urls.screenViewingURL).to.eq('screen-viewing-rewritten-url');
+      expect(urls.signalingURL).to.eq('signaling-rewritten-url');
+      expect(urls.turnControlURL).to.eq('turn-control-rewritten-url');
+    });
+  });
+});
