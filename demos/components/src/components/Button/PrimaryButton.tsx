ae3ef017432fae9a83380cd3c6f3149a289a937b
Dave Lane <landave@amazon.com>
2020-04-09 12:15:57 -0700

    Removed incomplete /react and /components directories

--- demos/components/src/components/Button/PrimaryButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { ButtonProps, Button } from './';
-import { StyledPrimaryButton } from './Styled';
-
-export const PrimaryButton: React.FC<ButtonProps> = (props) => <Button addStyles={StyledPrimaryButton} {...props} />;
-
-export default PrimaryButton;
