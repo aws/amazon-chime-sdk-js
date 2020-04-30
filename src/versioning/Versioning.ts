5db1dbb616213a7d54a945eceb752dc90b319c43
Kashish Tayal <katayal@amazon.com>
2020-04-29 19:11:34 -0700

    Fix sip url for meeting demo

--- src/versioning/Versioning.ts
+++ src/versioning/Versioning.ts
@@ -11,7 +11,7 @@ export default class Versioning {
    * Return string representation of SDK version
    */
   static get sdkVersion(): string {
-    return '1.4.4';
+    return '1.4.5';
   }
 
   /**
