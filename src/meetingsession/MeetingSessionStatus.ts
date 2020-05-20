nakshathru/amazon-chime-es-sdk

Mark 403 (Forbidden) for fetching turn credentials as terminal error and avoid retrying.

src/meetingsession/MeetingSessionStatus.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import { SdkSignalFrame } from '../signalingprotocol/SignalingProtocol.js';
@@ -45,6 +45,7 @@ export default class MeetingSessionStatus {
       case MeetingSessionStatusCode.AudioCallEnded:
       case MeetingSessionStatusCode.AudioInternalServerError:
       case MeetingSessionStatusCode.AudioDisconnected:
+      case MeetingSessionStatusCode.ForbiddenToFetchTurnCredentials:
       case MeetingSessionStatusCode.SignalingBadRequest:
       case MeetingSessionStatusCode.SignalingInternalServerError:
       case MeetingSessionStatusCode.SignalingRequestFailed:

test/meetingsession/MeetingSessionStatus.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import * as chai from 'chai';
@@ -25,6 +25,7 @@ describe('MeetingSessionStatus', () => {
     MeetingSessionStatusCode.AudioInternalServerError,
     MeetingSessionStatusCode.AudioServiceUnavailable,
     MeetingSessionStatusCode.AudioDisconnected,
+    MeetingSessionStatusCode.ForbiddenToFetchTurnCredentials,
     MeetingSessionStatusCode.VideoCallSwitchToViewOnly,
     MeetingSessionStatusCode.VideoCallAtSourceCapacity,
     MeetingSessionStatusCode.SignalingBadRequest,
@@ -70,6 +71,7 @@ describe('MeetingSessionStatus', () => {
         MeetingSessionStatusCode.AudioCallEnded,
         MeetingSessionStatusCode.AudioInternalServerError,
         MeetingSessionStatusCode.AudioDisconnected,
+        MeetingSessionStatusCode.ForbiddenToFetchTurnCredentials,
         MeetingSessionStatusCode.SignalingBadRequest,
         MeetingSessionStatusCode.SignalingInternalServerError,
         MeetingSessionStatusCode.SignalingRequestFailed,

test/task/ReceiveTURNCredentialsTask.test.ts
@@ -7,6 +7,7 @@ import AudioVideoControllerState from '../../src/audiovideocontroller/AudioVideo
 import NoOpAudioVideoController from '../../src/audiovideocontroller/NoOpAudioVideoController';
 import MeetingSessionConfiguration from '../../src/meetingsession/MeetingSessionConfiguration';
 import MeetingSessionCredentials from '../../src/meetingsession/MeetingSessionCredentials';
+import MeetingSessionStatusCode from '../../src/meetingsession/MeetingSessionStatusCode';
 import MeetingSessionTURNCredentials from '../../src/meetingsession/MeetingSessionTURNCredentials';
 import MeetingSessionURLs from '../../src/meetingsession/MeetingSessionURLs';
 import TimeoutScheduler from '../../src/scheduler/TimeoutScheduler';
@@ -67,6 +68,20 @@ describe('ReceiveTURNCredentialsTask', () => {
           expect(context.turnCredentials).to.equal(null);
           done();
         });
+    });
+
+    it('handles a 403 error to fetch TURN Credentials', async () => {
+      domMockBehavior.fetchSucceeds = true;
+      domMockBehavior.responseStatusCode = 403;
+      domMockBehavior.responseSuccess = false;
+      try {
+        await task.run();
+        throw new Error('This line should not be reached.');
+      } catch (error) {
+        expect(error.message).includes(
+          `the meeting status code: ${MeetingSessionStatusCode.ForbiddenToFetchTurnCredentials}`
+        );
+      }
     });
 
     it('will bypass the task when a url is not specified', async () => {

aws/amazon-chime-sdk-js

src/meetingsession/MeetingSessionStatus.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import { SdkSignalFrame } from '../signalingprotocol/SignalingProtocol.js';
@@ -45,6 +45,7 @@ export default class MeetingSessionStatus {
       case MeetingSessionStatusCode.AudioCallEnded:
       case MeetingSessionStatusCode.AudioInternalServerError:
       case MeetingSessionStatusCode.AudioDisconnected:
+      case MeetingSessionStatusCode.TURNCredentialsForbidden:
       case MeetingSessionStatusCode.SignalingBadRequest:
       case MeetingSessionStatusCode.SignalingInternalServerError:
       case MeetingSessionStatusCode.SignalingRequestFailed:

test/meetingsession/MeetingSessionStatus.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import * as chai from 'chai';
@@ -31,6 +31,7 @@ describe('MeetingSessionStatus', () => {
     MeetingSessionStatusCode.SignalingInternalServerError,
     MeetingSessionStatusCode.SignalingRequestFailed,
     MeetingSessionStatusCode.StateMachineTransitionFailed,
+    MeetingSessionStatusCode.TURNCredentialsForbidden,
     MeetingSessionStatusCode.ICEGatheringTimeoutWorkaround,
     MeetingSessionStatusCode.ConnectionHealthReconnect,
     MeetingSessionStatusCode.RealtimeApiFailed,
@@ -73,6 +74,7 @@ describe('MeetingSessionStatus', () => {
         MeetingSessionStatusCode.SignalingBadRequest,
         MeetingSessionStatusCode.SignalingInternalServerError,
         MeetingSessionStatusCode.SignalingRequestFailed,
+        MeetingSessionStatusCode.TURNCredentialsForbidden,
         MeetingSessionStatusCode.VideoCallAtSourceCapacity,
         MeetingSessionStatusCode.RealtimeApiFailed,
       ]);

test/task/ReceiveTURNCredentialsTask.test.ts
@@ -7,6 +7,7 @@ import AudioVideoControllerState from '../../src/audiovideocontroller/AudioVideo
 import NoOpAudioVideoController from '../../src/audiovideocontroller/NoOpAudioVideoController';
 import MeetingSessionConfiguration from '../../src/meetingsession/MeetingSessionConfiguration';
 import MeetingSessionCredentials from '../../src/meetingsession/MeetingSessionCredentials';
+import MeetingSessionStatusCode from '../../src/meetingsession/MeetingSessionStatusCode';
 import MeetingSessionTURNCredentials from '../../src/meetingsession/MeetingSessionTURNCredentials';
 import MeetingSessionURLs from '../../src/meetingsession/MeetingSessionURLs';
 import TimeoutScheduler from '../../src/scheduler/TimeoutScheduler';
@@ -67,6 +68,20 @@ describe('ReceiveTURNCredentialsTask', () => {
           expect(context.turnCredentials).to.equal(null);
           done();
         });
+    });
+
+    it('handles a 403 error to fetch TURN Credentials', async () => {
+      domMockBehavior.fetchSucceeds = true;
+      domMockBehavior.responseStatusCode = 403;
+      domMockBehavior.responseSuccess = false;
+      try {
+        await task.run();
+        throw new Error('This line should not be reached.');
+      } catch (error) {
+        expect(error.message).includes(
+          `the meeting status code: ${MeetingSessionStatusCode.TURNCredentialsForbidden}`
+        );
+      }
     });
 
     it('will bypass the task when a url is not specified', async () => {