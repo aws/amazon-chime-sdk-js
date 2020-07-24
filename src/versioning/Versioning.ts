992e3dd060e89546c4942cc3e26996167b9d2703
Anuran Duttaroy <anurand@amazon.com>
2020-07-24 14:26:33 -0700

    Fixed CHANGELOG entry for 2 previous commits

--- src/versioning/Versioning.ts
+++ src/versioning/Versioning.ts
@@ -18,7 +18,7 @@ export default class Versioning {
    * Return string representation of SDK version
    */
   static get sdkVersion(): string {
-    return '1.13.4';
+    return '1.13.5';
   }
 
   /**
