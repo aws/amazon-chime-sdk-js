3f174fb6c53c6b6ad2d6816fd6fa21d9b60ae58a
Ridip De <ridip@amazon.com>
2020-08-20 18:06:24 -0700

    Publish amazon-chime-sdk-js@1.16.0

--- src/versioning/Versioning.ts
+++ src/versioning/Versioning.ts
@@ -18,7 +18,7 @@ export default class Versioning {
    * Return string representation of SDK version
    */
   static get sdkVersion(): string {
-    return '1.15.5';
+    return '1.16.0';
   }
 
   /**
