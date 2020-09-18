3a0cd2e5a9cbe9476ce1e0ad1ac3f93932535b9b
Venkatesh Devale <devalevd@amazon.com>
2020-09-18 16:47:00 -0700

    Add documentation for DefaultActiveSpeakerPolicy constructor

--- src/versioning/Versioning.ts
+++ src/versioning/Versioning.ts
@@ -18,7 +18,7 @@ export default class Versioning {
    * Return string representation of SDK version
    */
   static get sdkVersion(): string {
-    return '1.17.13';
+    return '1.17.14';
   }
 
   /**
