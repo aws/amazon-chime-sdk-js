df4e60cbf3f17c45953670939e43ebb765d82f22
Anuran Duttaroy <anurand@amazon.com>
2020-09-30 16:49:45 -0700

    Replace `awesome-typescript-loader` with `ts-loader`

--- demos/browser/webpack.config.js
+++ demos/browser/webpack.config.js
@@ -65,7 +65,7 @@ module.exports = env => {
         },
         {
           test: /\.tsx?$/,
-          loader: 'awesome-typescript-loader',
+          loader: 'ts-loader',
         },
       ],
     },
