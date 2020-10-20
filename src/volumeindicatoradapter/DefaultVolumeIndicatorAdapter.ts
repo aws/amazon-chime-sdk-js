ea2f62f00954d3e53f838e8ae5c7217b60d0f031
Andrew Giel <andrew@ubiquity6.com>
2020-10-20 16:21:58 -0400

    check for valid attendeeId when setting presence from volume indicator adaptor

--- src/volumeindicatoradapter/DefaultVolumeIndicatorAdapter.ts
+++ src/volumeindicatoradapter/DefaultVolumeIndicatorAdapter.ts
@@ -72,7 +72,7 @@ export default class DefaultVolumeIndicatorAdapter implements VolumeIndicatorAda
             break;
           }
         }
-        if (!attendeeHasNewStreamId) {
+        if (!attendeeHasNewStreamId && !!attendeeId) {
           this.realtimeController.realtimeSetAttendeeIdPresence(
             attendeeId,
             false,
