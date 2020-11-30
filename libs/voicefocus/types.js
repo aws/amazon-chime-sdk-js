c2c3e65d805d01c2f5f85b4ee4fc57c99029c58e
Romain Lebossé <romain.lebosse@klaxoon.com>
2020-11-30 18:41:45 +0100

    Fix "globalThis is not defined" error

--- libs/voicefocus/types.js
+++ libs/voicefocus/types.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VoiceFocusAudioWorkletNode = void 0;
-class VoiceFocusAudioWorkletNode extends (globalThis['AudioWorkletNode'] || class Sadness {
+class VoiceFocusAudioWorkletNode extends ((typeof globalThis !== 'undefined' && globalThis['AudioWorkletNode']) || class Sadness {
 }) {
 }
 exports.VoiceFocusAudioWorkletNode = VoiceFocusAudioWorkletNode;
