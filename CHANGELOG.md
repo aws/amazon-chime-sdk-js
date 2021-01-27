9f3eaa1c1615e0d12115d4f56b4d722beedbbf99
Xuan He <46945302+XHatan@users.noreply.github.com>
2021-01-26 18:57:19 -0800

    Add demo for tensorflow bodypix (#1027)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Added
+- [DEMO] Add a image segmentation video filter based on Tensorflow BodyPix
 
 
 ### Changed
--- demos/browser/app/meetingV2/videofilter/CircularCut.ts
+++ demos/browser/app/meetingV2/videofilter/CircularCut.ts
@@ -1,11 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-  CanvasVideoFrameBuffer,
-  VideoFrameBuffer,
-  VideoFrameProcessor,
-} from 'amazon-chime-sdk-js';
+import { CanvasVideoFrameBuffer, VideoFrameBuffer, VideoFrameProcessor } from 'amazon-chime-sdk-js';
 
 /**
  * [[CircularCut]] is an implementation of {@link VideoFrameProcessor} for demonstration purpose.
