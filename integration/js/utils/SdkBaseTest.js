a9d3368815c510c996fa13d5a0208fecce37a8f3
michhyun <michhyun@amazon.com>
2021-04-08 07:46:02 -0700

    Revert "Fix URL generation (#1185)"
    This reverts commit f1db34694b8e8c92f623a902edb40968a4ea5cdb.

--- integration/js/utils/SdkBaseTest.js
+++ integration/js/utils/SdkBaseTest.js
@@ -7,14 +7,13 @@ const {LocalSession} = require('./WebdriverLocal');
 const {emitMetric} = require('./CloudWatch');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const { getTransformedURL } = require('./util');
 
 class SdkBaseTest extends KiteBaseTest {
   constructor(name, kiteConfig, testName) {
     super(name, kiteConfig);
     this.baseUrl = this.url;
     if (testName === 'ContentShareOnlyAllowTwoTest') {
-      this.url = getTransformedURL(this.url, 'max-content-share', 'true');
+      this.url += '?max-content-share=true';
     }
 
     if (testName === 'MessagingSessionTest') {
@@ -81,7 +80,7 @@ class SdkBaseTest extends KiteBaseTest {
       this.io.on("meeting_created", meetingId => {
         this.meetingCreated = true;
         this.meetingTitle = meetingId;
-        this.url = getTransformedURL(this.originalURL, 'm', this.meetingTitle);
+        this.url = this.originalURL + '?m=' + this.meetingTitle;
       });
       this.io.on("finished", () => {
         this.testFinish = true;
@@ -108,7 +107,11 @@ class SdkBaseTest extends KiteBaseTest {
       this.io.emit("setup_test", this.baseUrl, this.attendeeId);
     } else {
       this.meetingTitle = uuidv4();
-      this.url = getTransformedURL(this.originalURL, 'm', this.meetingTitle);
+      if (this.originalURL.indexOf('?') !== -1) {
+        this.url = this.originalURL + '&m=' + this.meetingTitle;
+      } else {
+        this.url = this.originalURL + '?m=' + this.meetingTitle;
+      }
     }
   }
 
