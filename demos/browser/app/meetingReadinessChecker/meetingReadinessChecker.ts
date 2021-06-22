6670e2e87ff8ec4d89a1efd3723ee5fda5c692ff
Lu Dai <dailu@amazon.com>
2021-06-22 14:39:52 -0700

    Update setting media region to handle beta stage for meeting readiness checker app

--- demos/browser/app/meetingReadinessChecker/meetingReadinessChecker.ts
+++ demos/browser/app/meetingReadinessChecker/meetingReadinessChecker.ts
@@ -608,8 +608,16 @@ export class DemoMeetingApp {
   setMediaRegion(): void {
     new AsyncScheduler().start(
       async (): Promise<void> => {
+        let nearestMediaRegion = null;
         try {
-          const nearestMediaRegion = await this.getNearestMediaRegion();
+          const query = new URLSearchParams(document.location.search);
+          const stage = query.get('stage');
+          if (stage === 'beta') {
+            const regions = ['us-east-1', 'ap-south-1', 'us-west-2'];
+            nearestMediaRegion = regions[Math.floor(Math.random()*regions.length)];
+          } else {
+          nearestMediaRegion = await this.getNearestMediaRegion();
+          }
           if (nearestMediaRegion === '' || nearestMediaRegion === null) {
             throw new Error('Nearest Media Region cannot be null or empty');
           }
