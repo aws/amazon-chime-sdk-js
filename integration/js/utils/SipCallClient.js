71a157f90de614285b701b8f0bdff32c9c4d65c3
Kashish Tayal <katayal@amazon.com>
2021-06-23 14:08:13 -0700

    fixing method name in sip call test

--- integration/js/utils/SipCallClient.js
+++ integration/js/utils/SipCallClient.js
@@ -8,7 +8,7 @@ class SipCallClient {
     this.resultPath = resultPath;
   }
 
-  voiceConnectorIdcall(voiceConnectorId, joinToken, meetingTitle) {
+  call(voiceConnectorId, joinToken, meetingTitle) {
     console.log(`Join Token : ${joinToken.split("X-joinToken=")[1]}`);
     return exec(`env TERMINATE_ON_BYE=1 DISABLE_DTMF=1 PLAY_TONE=1 TONE_PATH=${this.sipTestAssetPath}/test_tone.wav SIP_DOMAIN="${voiceConnectorId}.voiceconnector.chime.aws;transport=tls;X-chime-join-token=${joinToken.split("X-joinToken=")[1]}" SIP_USER=29709308 SIP_PASSWD=Tz6cghdEgKA4 SIP_CALLER_ID=${this.getSourcePhoneNumber()} SIP_PUBLIC_IP_ADDR=3.227.216.243 ${this.sipTestAssetPath}/sipagent_tls +17035550122 12345 ${this.resultPath}/test.wav`, (error, stdout, stderr) => {
       if (error) {
