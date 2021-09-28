e7ff318a89fb58159d36b715e7bbe8c4d28d619f
Venkatesh Devale <61809205+devalevenkatesh@users.noreply.github.com>
2021-09-27 17:30:44 -0700

    Add additional logging for video check by attendee name (#1619)
    * Add additional logging for video check by attendee name
    
    * Add Chrome capabilities and turn off extended debugging on tests
    
    * Add debugging back
    
    * Add more logging

--- integration/js/pages/AppPage.js
+++ integration/js/pages/AppPage.js
@@ -569,8 +569,10 @@ class AppPage {
   }
 
   async videoCheckByAttendeeName(stepInfo, attendeeName, expectedState = 'video') {
+    const startTime = Date.now();
     let checked;
     let videos = await this.driver.findElements(By.css('video[id^="video-"]'));
+    console.log(`Looping through ${videos && videos.length}`);
     for (let i = 0; i < videos.length; i++) {
       const videoElementId = await videos[i].getAttribute('id');
       const seperatorIndex = videoElementId.lastIndexOf("-");
@@ -578,21 +580,26 @@ class AppPage {
         const tileIndex = parseInt(videoElementId.slice(seperatorIndex+1))
         if (tileIndex != NaN && tileIndex >= 0) {
           const nameplate = await this.driver.findElement(By.id(`nameplate-${tileIndex}`));
+          console.log(`nameplate: ${nameplate}`);
           const nameplateText = await nameplate.getText();
           if (nameplate && nameplateText === attendeeName) {
             let numRetries = 10;
             let retry = 0;
+            console.log(`Start verifying video display by video element ID:${videoElementId}, attendeeName=${attendeeName}, tileIndex=${tileIndex}`);
             let checked = await TestUtils.verifyVideoDisplayById(stepInfo.driver, `video-${tileIndex}`);
             while ((checked.result !== expectedState) && retry < numRetries) {
+              console.log(`video check not yet complete, retrying again, retry count: ${retry}`);
               checked = await TestUtils.verifyVideoDisplayById(stepInfo.driver, `video-${tileIndex}`);
               retry++;
               await TestUtils.waitAround(1000);
             }
+            console.log(`videoCheckByAttendeeName completed in: ${Date.now()-startTime}ms`);
             return checked.result;
           }
         }
       }
     }
+    console.log(`videoCheckByAttendeeName completed in: ${Date.now()-startTime}ms`);
     return 'blank';
   }
 
