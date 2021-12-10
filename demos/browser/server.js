1e1650bf9d8c233a3735d64383ecd111b9405339
Venkatesh Devale <devalevd@amazon.com>
2021-12-10 15:07:47 -0800

    Optional chaining is not supported in Node < v14. Hence, fallback to older syntax.
    We still support Node 12 in demo README and the JS SDK also has engine field with node12+
    support.

--- demos/browser/server.js
+++ demos/browser/server.js
@@ -49,7 +49,9 @@ if (captureS3Destination) {
 
 // return Chime Meetings SDK Client just for Echo Reduction for now.
 function getClientForMeeting(meeting) {
-  return ((useChimeSDKMeetings === 'true') || (meeting?.Meeting?.MeetingFeatures?.Audio?.EchoReduction === 'AVAILABLE')) ? chimeSDKMeetings : chime;
+  return (
+    (useChimeSDKMeetings === 'true') ||
+    (meeting && meeting.Meeting && meeting.Meeting.MeetingFeatures && meeting.Meeting.MeetingFeatures.Audio && meeting.Meeting.MeetingFeatures.Audio.EchoReduction === 'AVAILABLE')) ? chimeSDKMeetings : chime;
 }
 
 function serve(host = '127.0.0.1:8080') {
