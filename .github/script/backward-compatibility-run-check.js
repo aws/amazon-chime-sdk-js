e022f466faa07ec151c7eab1d1402100f1e1601e
Venkatesh Devale <61809205+devalevenkatesh@users.noreply.github.com>
2022-03-02 09:17:20 -0800

    Fix backward compatibility check (#2077)
    * Fix backward compatibility check and add demo setup `js` script
    
    - Fixes backward compatibility check considering beta and first major version
    for any new major version.
    - Convert bash script from GitHub action to NodeJS script.
    
    * Use `process.exit` over `console.log` in GitHub action for backward run check
    
    - Use GitHub action conditional `if` functions `success()` to trigger
    the backward compatibility checks based on whether its a need to run them.
    - Use `process.exit`
    
    * Revert to `console.log` but with extra detailed log

---
+++ .github/script/backward-compatibility-run-check.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * This script simply checks whether the version we are releasing is either the first NPM version for any new major version or
+ * a beta version. For both, we do not want to run backward compatibility checks since both will have backward in-compatible breaking changes.
+ * 
+ * For e.g: 3.0.0, 3.0.0-beta.0, 4.0.0 and so on.
+ */
+ const { spawnOrFail } = require('../../script/cli-utils');
+
+ // eslint-disable-next-line @typescript-eslint/no-var-requires
+const versionToRelease = require('../../package.json').version;
+const preReleaseName = (spawnOrFail('node', ['.github/script/get-pre-release-name'], { skipOutput: true })).trim();
+
+// Check version to release if is a pre-release version or an first major version of a new major version.
+// This satisfies versions like: 3.0.0, 4.0.0, 3.0.0-beta.0, 3.0.0-beta.1
+if (versionToRelease.includes(preReleaseName)) {
+  console.log(`Skip backward compatibility checks for a pre release ${preReleaseName} version: ${versionToRelease}`);
+} else if ((/^[0-9]+\.0\.0$/g).test(versionToRelease)) {
+  console.log(`Skip backward compatibility checks for a new major version: ${versionToRelease}`);
+} else {
+  console.log(`Run backward compatibility checks to release ${versionToRelease}`);
+}
