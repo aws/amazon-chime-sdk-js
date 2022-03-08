39167f0794b054f86bb51be2f1dc11b6a54fe457
hekmat barbar <hekmatb@amazon.com>
2022-03-08 13:18:12 -0800

    update integration tests pre-run script ID

--- integration/js/utils/WebdriverSauceLabs.js
+++ integration/js/utils/WebdriverSauceLabs.js
@@ -104,7 +104,7 @@ const getPrerunScript = (capabilities) =>{
   const name = capabilities.name;
   const blurName = "Background Blur Test";
   const repName = "Background Replacement Test";
-  return (name.includes(blurName) || name.includes(repName)) ?  'storage:8f86b53d-eeb3-4eb3-98c5-4c2b79544d23' : "";
+  return (name.includes(blurName) || name.includes(repName)) ?  'storage:f196801d-998f-4ef2-bc2b-059b8521f345' : "";
 }
 const getChromeCapabilities = capabilities => {
   let cap = Capabilities.chrome();
