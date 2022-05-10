aa9774646a76683cb3d6c930a59821b4477df2b7
Akash Priyadarshi <akashpri@amazon.com>
2022-05-09 23:49:14 -0700

    Logging meeting id for integration tests

--- integration/js/pages/AppPage.js
+++ integration/js/pages/AppPage.js
@@ -71,7 +71,8 @@ function findAllElements() {
     echoReductionFeature: By.id('echo-reduction-capability'),
     echoReductionFeatureLabel: By.css('label[for="echo-reduction-capability"]'),
     stereoMusicProfileCheckBox: By.id('fullband-music-stereo-quality'),
-    stereoMusicProfileCheckBoxLabel: By.css('label[for="fullband-music-stereo-quality"]')
+    stereoMusicProfileCheckBoxLabel: By.css('label[for="fullband-music-stereo-quality"]'),
+    chimeMeetingId: By.id('chime-meeting-id')
   };
 }
 
@@ -948,6 +949,12 @@ class AppPage {
       return true;
     }
     return false;
+  }
+
+  async logMeetingId() {
+    const meetingElement = this.driver.findElement(elements.chimeMeetingId);
+    const meetingId = await meetingElement.getText();
+    this.logger(`Meeting started for ${meetingId}`);
   }
 }
 
