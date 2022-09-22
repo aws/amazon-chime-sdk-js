3a0e0309dd0f03d4e526d09a8e5e7966cfe3489a
Venkatesh Devale <61809205+devalevenkatesh@users.noreply.github.com>
2022-09-21 18:45:26 -0700

    Remove more console logs which should not be present when the github action runs (#2451)

--- .github/script/backward-compatibility-run-check.js
+++ .github/script/backward-compatibility-run-check.js
@@ -10,9 +10,7 @@
 
  // eslint-disable-next-line @typescript-eslint/no-var-requires
 const versionToRelease = require('../../package.json').version;
-console.log('versionToRelease', versionToRelease);
 const preReleaseName = (spawnOrFail('node', ['.github/script/get-pre-release-name'], { skipOutput: true })).trim();
-console.log('preReleaseName', preReleaseName);
 // Check version to release if is a pre-release version or an first major version of a new major version.
 // This satisfies versions like: 3.0.0, 4.0.0, 3.0.0-beta.0, 3.0.0-beta.1
 if (preReleaseName && versionToRelease.includes(preReleaseName)) {
