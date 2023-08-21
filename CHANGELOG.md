20f2e1aa4c813ec1a885f581da0cb79c556d62c8
Shi Su <67605788+shi-su@users.noreply.github.com>
2023-08-21 13:15:11 -0700

    Receive notification messages from server and print received messages to console (#2731)
    Co-authored-by: Shi Su <shisuss@amazon.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Allow receiving notification messages from server and print messages received to console
+
 ### Removed
 
 ### Changed
--- src/signalingprotocol/SignalingProtocol.d.ts
+++ src/signalingprotocol/SignalingProtocol.d.ts
@@ -71,6 +71,9 @@ export interface ISdkSignalFrame {
 
     /** SdkSignalFrame primaryMeetingLeave */
     primaryMeetingLeave?: (ISdkPrimaryMeetingLeaveFrame|null);
+
+    /** SdkSignalFrame notification */
+    notification?: (ISdkNotificationFrame|null);
 }
 
 /** Represents a SdkSignalFrame. */
@@ -150,6 +153,9 @@ export class SdkSignalFrame implements ISdkSignalFrame {
 
     /** SdkSignalFrame primaryMeetingLeave. */
     public primaryMeetingLeave?: (ISdkPrimaryMeetingLeaveFrame|null);
+
+    /** SdkSignalFrame notification. */
+    public notification?: (ISdkNotificationFrame|null);
 
     /**
      * Creates a new SdkSignalFrame instance using the specified properties.
@@ -246,7 +252,8 @@ export namespace SdkSignalFrame {
         REMOTE_VIDEO_UPDATE = 24,
         PRIMARY_MEETING_JOIN = 25,
         PRIMARY_MEETING_JOIN_ACK = 26,
-        PRIMARY_MEETING_LEAVE = 27
+        PRIMARY_MEETING_LEAVE = 27,
+        NOTIFICATION = 34
     }
 }
 
@@ -4713,6 +4720,112 @@ export class SdkVideoSubscriptionConfiguration implements ISdkVideoSubscriptionC
      * @returns JSON object
      */
     public toJSON(): { [k: string]: any };
+}
+
+/** Properties of a SdkNotificationFrame. */
+export interface ISdkNotificationFrame {
+
+    /** SdkNotificationFrame level */
+    level?: (SdkNotificationFrame.NotificationLevel|null);
+
+    /** SdkNotificationFrame message */
+    message?: (string|null);
+}
+
+/** Represents a SdkNotificationFrame. */
+export class SdkNotificationFrame implements ISdkNotificationFrame {
+
+    /**
+     * Constructs a new SdkNotificationFrame.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: ISdkNotificationFrame);
+
+    /** SdkNotificationFrame level. */
+    public level: SdkNotificationFrame.NotificationLevel;
+
+    /** SdkNotificationFrame message. */
+    public message: string;
+
+    /**
+     * Creates a new SdkNotificationFrame instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns SdkNotificationFrame instance
+     */
+    public static create(properties?: ISdkNotificationFrame): SdkNotificationFrame;
+
+    /**
+     * Encodes the specified SdkNotificationFrame message. Does not implicitly {@link SdkNotificationFrame.verify|verify} messages.
+     * @param message SdkNotificationFrame message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: ISdkNotificationFrame, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified SdkNotificationFrame message, length delimited. Does not implicitly {@link SdkNotificationFrame.verify|verify} messages.
+     * @param message SdkNotificationFrame message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: ISdkNotificationFrame, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a SdkNotificationFrame message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns SdkNotificationFrame
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): SdkNotificationFrame;
+
+    /**
+     * Decodes a SdkNotificationFrame message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns SdkNotificationFrame
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): SdkNotificationFrame;
+
+    /**
+     * Verifies a SdkNotificationFrame message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a SdkNotificationFrame message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns SdkNotificationFrame
+     */
+    public static fromObject(object: { [k: string]: any }): SdkNotificationFrame;
+
+    /**
+     * Creates a plain object from a SdkNotificationFrame message. Also converts values to other types if specified.
+     * @param message SdkNotificationFrame
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: SdkNotificationFrame, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this SdkNotificationFrame to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+}
+
+export namespace SdkNotificationFrame {
+
+    /** NotificationLevel enum. */
+    enum NotificationLevel {
+        INFO = 1,
+        WARNING = 2,
+        ERROR = 3
+    }
 }
 
 /** Properties of a SdkPrimaryMeetingJoinFrame. */
