d6487f4339d558956d66cd93579cc34f5ce04f7c
Henry Smith <36210679+hensmi-amazon@users.noreply.github.com>
2024-01-25 15:01:10 -0800

    Add support for H.264 profiles besides Constrained Baseline Profile (#2830)

--- test/sdp/SDPMock.ts
+++ test/sdp/SDPMock.ts
@@ -5652,7 +5652,7 @@ a=rtcp-fb:125 transport-cc\r
 a=rtcp-fb:125 ccm fir\r
 a=rtcp-fb:125 nack\r
 a=rtcp-fb:125 nack pli\r
-a=fmtp:125 \r
+a=fmtp:125\r
 a=rtpmap:107 rtx/90000\r
 a=fmtp:107 apt=125\r
 a=rtpmap:108 H264/90000\r
@@ -5768,5 +5768,173 @@ a=rtpmap:113 telephone-event/16000\r
 a=rtpmap:126 telephone-event/8000\r
 a=ssrc:4012659083 cname:ZP28PpLAHaoSlS7P\r
 a=ssrc:4012659083 msid:cd932a75-0354-4253-ad7d-e36f2414733d 8d0e41a3-f4d5-488d-8296-5960bd1d70a0\r
+`;
+
+  static readonly LOCAL_OFFER_WITH_MULTIPLE_H264_PROFILES = `v=0\r
+o=- 1672124140717632411 2 IN IP4 127.0.0.1\r
+s=-\r
+t=0 0\r
+a=group:BUNDLE 0 1 2\r
+a=extmap-allow-mixed\r
+a=msid-semantic: WMS f3dac474-f5f8-48c0-b0f6-114cfdac587b\r
+m=audio 9 UDP/TLS/RTP/SAVPF 111 63 9 0 8 13 110 126\r
+c=IN IP4 0.0.0.0\r
+a=rtcp:9 IN IP4 0.0.0.0\r
+a=ice-ufrag:jgnF\r
+a=ice-pwd:7ja8PGQpp7m6PqqxUOeizxUg\r
+a=ice-options:trickle\r
+a=fingerprint:sha-256 6A:8B:43:7D:CA:39:72:5A:2D:51:95:19:A9:0A:82:31:A2:63:1A:3D:E8:54:1D:15:FE:A0:37:3C:2C:36:DA:18\r
+a=setup:actpass\r
+a=mid:0\r
+a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r
+a=sendrecv\r
+a=msid:f3dac474-f5f8-48c0-b0f6-114cfdac587b 06d8d1c6-49b6-45c0-8cd0-33586b0349f7\r
+a=rtcp-mux\r
+a=rtpmap:111 opus/48000/2\r
+a=rtcp-fb:111 transport-cc\r
+a=fmtp:111 minptime=10;useinbandfec=1\r
+a=rtpmap:63 red/48000/2\r
+a=fmtp:63 111/111\r
+a=rtpmap:9 G722/8000\r
+a=rtpmap:0 PCMU/8000\r
+a=rtpmap:8 PCMA/8000\r
+a=rtpmap:13 CN/8000\r
+a=rtpmap:110 telephone-event/48000\r
+a=rtpmap:126 telephone-event/8000\r
+a=ssrc:3405851280 cname:MgBxgJN8OLIit3KO\r
+a=ssrc:3405851280 msid:f3dac474-f5f8-48c0-b0f6-114cfdac587b 06d8d1c6-49b6-45c0-8cd0-33586b0349f7\r
+m=video 9 UDP/TLS/RTP/SAVPF 96 97 102 103 104 105 106 107 108 109 127 125 39 40 45 46 98 99 100 101 112 113 116 117 118\r
+c=IN IP4 0.0.0.0\r
+a=rtcp:9 IN IP4 0.0.0.0\r
+a=ice-ufrag:jgnF\r
+a=ice-pwd:7ja8PGQpp7m6PqqxUOeizxUg\r
+a=ice-options:trickle\r
+a=fingerprint:sha-256 6A:8B:43:7D:CA:39:72:5A:2D:51:95:19:A9:0A:82:31:A2:63:1A:3D:E8:54:1D:15:FE:A0:37:3C:2C:36:DA:18\r
+a=setup:actpass\r
+a=mid:1\r
+a=extmap:14 urn:ietf:params:rtp-hdrext:toffset\r
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r
+a=extmap:13 urn:3gpp:video-orientation\r
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r
+a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay\r
+a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type\r
+a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing\r
+a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space\r
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r
+a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r
+a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r
+a=sendrecv\r
+a=msid:f3dac474-f5f8-48c0-b0f6-114cfdac587b b52e6030-df95-4b6e-84e4-abdb12a6d489\r
+a=rtcp-mux\r
+a=rtcp-rsize\r
+a=rtpmap:96 VP8/90000\r
+a=rtcp-fb:96 goog-remb\r
+a=rtcp-fb:96 transport-cc\r
+a=rtcp-fb:96 ccm fir\r
+a=rtcp-fb:96 nack\r
+a=rtcp-fb:96 nack pli\r
+a=rtpmap:97 rtx/90000\r
+a=fmtp:97 apt=96\r
+a=rtpmap:102 H264/90000\r
+a=rtcp-fb:102 goog-remb\r
+a=rtcp-fb:102 transport-cc\r
+a=rtcp-fb:102 ccm fir\r
+a=rtcp-fb:102 nack\r
+a=rtcp-fb:102 nack pli\r
+a=fmtp:102 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f\r
+a=rtpmap:103 rtx/90000\r
+a=fmtp:103 apt=102\r
+a=rtpmap:104 H264/90000\r
+a=rtcp-fb:104 goog-remb\r
+a=rtcp-fb:104 transport-cc\r
+a=rtcp-fb:104 ccm fir\r
+a=rtcp-fb:104 nack\r
+a=rtcp-fb:104 nack pli\r
+a=fmtp:104 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f\r
+a=rtpmap:105 rtx/90000\r
+a=fmtp:105 apt=104\r
+a=rtpmap:106 H264/90000\r
+a=rtcp-fb:106 goog-remb\r
+a=rtcp-fb:106 transport-cc\r
+a=rtcp-fb:106 ccm fir\r
+a=rtcp-fb:106 nack\r
+a=rtcp-fb:106 nack pli\r
+a=fmtp:106 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f\r
+a=rtpmap:107 rtx/90000\r
+a=fmtp:107 apt=106\r
+a=rtpmap:108 H264/90000\r
+a=rtcp-fb:108 goog-remb\r
+a=rtcp-fb:108 transport-cc\r
+a=rtcp-fb:108 ccm fir\r
+a=rtcp-fb:108 nack\r
+a=rtcp-fb:108 nack pli\r
+a=fmtp:108 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f\r
+a=rtpmap:109 rtx/90000\r
+a=fmtp:109 apt=108\r
+a=rtpmap:127 H264/90000\r
+a=rtcp-fb:127 goog-remb\r
+a=rtcp-fb:127 transport-cc\r
+a=rtcp-fb:127 ccm fir\r
+a=rtcp-fb:127 nack\r
+a=rtcp-fb:127 nack pli\r
+a=fmtp:127 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d001f\r
+a=rtpmap:125 rtx/90000\r
+a=fmtp:125 apt=127\r
+a=rtpmap:39 H264/90000\r
+a=rtcp-fb:39 goog-remb\r
+a=rtcp-fb:39 transport-cc\r
+a=rtcp-fb:39 ccm fir\r
+a=rtcp-fb:39 nack\r
+a=rtcp-fb:39 nack pli\r
+a=fmtp:39 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=4d001f\r
+a=rtpmap:40 rtx/90000\r
+a=fmtp:40 apt=39\r
+a=rtpmap:45 AV1/90000\r
+a=rtcp-fb:45 goog-remb\r
+a=rtcp-fb:45 transport-cc\r
+a=rtcp-fb:45 ccm fir\r
+a=rtcp-fb:45 nack\r
+a=rtcp-fb:45 nack pli\r
+a=rtpmap:46 rtx/90000\r
+a=fmtp:46 apt=45\r
+a=rtpmap:98 VP9/90000\r
+a=rtcp-fb:98 goog-remb\r
+a=rtcp-fb:98 transport-cc\r
+a=rtcp-fb:98 ccm fir\r
+a=rtcp-fb:98 nack\r
+a=rtcp-fb:98 nack pli\r
+a=fmtp:98 profile-id=0\r
+a=rtpmap:99 rtx/90000\r
+a=fmtp:99 apt=98\r
+a=rtpmap:100 VP9/90000\r
+a=rtcp-fb:100 goog-remb\r
+a=rtcp-fb:100 transport-cc\r
+a=rtcp-fb:100 ccm fir\r
+a=rtcp-fb:100 nack\r
+a=rtcp-fb:100 nack pli\r
+a=fmtp:100 profile-id=2\r
+a=rtpmap:101 rtx/90000\r
+a=fmtp:101 apt=100\r
+a=rtpmap:112 H264/90000\r
+a=rtcp-fb:112 goog-remb\r
+a=rtcp-fb:112 transport-cc\r
+a=rtcp-fb:112 ccm fir\r
+a=rtcp-fb:112 nack\r
+a=rtcp-fb:112 nack pli\r
+a=fmtp:112 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=64001f\r
+a=rtpmap:113 rtx/90000\r
+a=fmtp:113 apt=112\r
+a=rtpmap:116 red/90000\r
+a=rtpmap:117 rtx/90000\r
+a=fmtp:117 apt=116\r
+a=rtpmap:118 ulpfec/90000\r
+a=ssrc-group:FID 2748666460 3612566398\r
+a=ssrc:2748666460 cname:MgBxgJN8OLIit3KO\r
+a=ssrc:2748666460 msid:f3dac474-f5f8-48c0-b0f6-114cfdac587b b52e6030-df95-4b6e-84e4-abdb12a6d489\r
+a=ssrc:3612566398 cname:MgBxgJN8OLIit3KO\r
+a=ssrc:3612566398 msid:f3dac474-f5f8-48c0-b0f6-114cfdac587b b52e6030-df95-4b6e-84e4-abdb12a6d489\r
 `;
 }
