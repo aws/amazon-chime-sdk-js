4802176b25560d2f79994d34850b8d978aa81df7
Henry Smith <36210679+hensmi-amazon@users.noreply.github.com>
2024-03-27 09:25:16 -0700

    Update send-test-report.js to only send on success

--- .github/script/send-test-report.js
+++ .github/script/send-test-report.js
@@ -5,11 +5,13 @@ const axios = require('axios');
 
 var myArgs = process.argv.slice(2);
 
-axios.post(myArgs[0], {
-  'github_workflow_url': myArgs[1],
-  'browser_compatibility_tests_status': myArgs[2].toUpperCase()
-}, res => {
-  console.log(res);
-}, err => {
-  console.log(err);
-});
+if (myArgs[2].toUpperCase() !== 'SUCCESS') {
+  axios.post(myArgs[0], {
+    'github_workflow_url': myArgs[1],
+    'browser_compatibility_tests_status': myArgs[2].toUpperCase()
+  }).then(res => {
+    console.log(res.data);
+  }).catch(err => {
+    console.log(err);
+  });
+}
