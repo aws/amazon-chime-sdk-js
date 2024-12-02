16f6ae275853e36a64f7d605c81fa32803c545a2
AMAN GUPTA <aguptaaa@amazon.com>
2024-12-01 23:35:15 -0800

    Add interface for custom CDN paths for VideoFxProcessor

---
+++ src/videofx/VideoFxSpec.ts
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import VideoFxPaths from './VideoFxPaths';
+
+/**
+ * An interface to specify customizable resources such as asset paths that will be presented as an input to VideoFxProcessor
+ */
+export default interface VideoFxSpec {
+  /**
+   * Paths that will be loaded from CDN for video effects.
+   */
+  paths?: VideoFxPaths;
+}
